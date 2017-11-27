e00f88a4956fc34025d1bec2b938e09413a1ca3e
Dale <yl622@njit.edu>
2017-11-26 20:33:05 -0500

    Removed classMap.php and use auto setDir to to the same work in index

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
-    $classMap = require('classMap.php');
     use classes;
     //Bug report massage
     ini_set('display_errors', 'On');
     error_reporting(E_ALL);
     //autoload all classes
+    class classMap {
+        static public function setDir ($input) {
+            return str_replace('\\','/', $input) . '.php';
+        }
+    }
     class Manage {
         static public function autoload($class) {
-            global $classMap;
-            include $classMap[$class];
+            include classMap::setdir($class);
         }
     }
     //register all loaded classes
-
     spl_autoload_register(array('Manage', 'autoload'));
     define('DATABASE', 'yl622');
     define('USERNAME', 'yl622');
